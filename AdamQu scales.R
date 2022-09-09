5376b6631fad2538e1996a5edccc2e60f513b7c8
Rachel Zhu <rachel@Rachels-MacBook-Air-7.local>
2022-09-09 15:41:53 -0400

    actual final document

--- AdamQu scales.R
+++ AdamQu scales.R
@@ -3,19 +3,36 @@
 library(ggplot2)
 library(tidyverse)
 
+#A dat variable containing the scales dataset.
 dat <- read.csv("scales.csv")
-dim(dat)
-head(dat)
-class(dat$N)
-class(dat$quadrant)
-class(dat$species)
-class(dat$specimen)
+
+#A line of code which reports the class of each column in the dataset.
 sapply(dat,class)
-dat$species <- as.factor(dat$species)
-species <- levels(dat$species)
-species
-length(species)
-#^How many unique values there are in "species"
-dat$species==species[1]
-#^Do the values in dat$species equal the first value in the "species" variable
+
+#A line of code which reports the dimensions of the dataset.
+dim(dat)
+
+#Code that produces a summary of the number of scales punctured for each species.
+species.n<- dat %>%
+  group_by(species) %>%
+  summarise(n = n())
+species.n
+
+#Code that produces a summary of the number of specimens sampled for each species.
+dat %>% 
+  count(species,specimen) %>%
+  print() %>%
+  count(species,name = "n.specimens")
+
+#Code that produces a PDF file containing 6 figures, one for each species that includes a boxplot of puncture force verus quadrant.
+pdf("species.quadrant.pdf")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()
+
+
 
