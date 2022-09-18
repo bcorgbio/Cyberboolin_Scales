481052358e117459e343f29e9afd9bffb0ca62a2
Rachel Zhu <rachel@rachels-air-8.bc.edu>
2022-09-18 15:39:56 -0400

    Project 2 update 3:39

--- CyberBoolin_Module2.R
+++ CyberBoolin_Module2.R
@@ -21,11 +21,16 @@ pseed2 <- pseed2%>%
 pseed.wide <- pseed2 %>%
   select(-amp)%>%
   pivot_wider(names_from = fin,values_from = amp.bl) %>%
-  mutate(amp.sum=L+R)%>%
-  print(amp.sum)
+  mutate(amp.sum= L+R)
+  
+view(pseed.wide)
+
+
 
 
 #Compute the mean maximum* of all the amp.sums for each specific swimming speed for each fish just like we did for mean maximum amplitude of each fin (i.e., the mean of all the max amplitudes across each experiment for each fish). Make sure this is contained in a new tibble named pseed.sum.max. Call this column amp.sum.mean.
+
+
 
 find.peaks <- function(x,y,mult=100){
   f <- fget(features(x = x,y=y*mult))[2:3]%>% 
@@ -35,10 +40,13 @@ find.peaks <- function(x,y,mult=100){
   return(f$peaks)
 }
 
-pseed.max <- pseed2%>%
+pseed.max <- pseed.wide %>%
   group_by(fish,speed)%>%
   mutate(peak=frame %in% find.peaks(frame,amp.sum))%>%
   filter(peak==T) 
+pseed.max$peak <- NULL
+
+
 
 pseed.sum.max<- pseed.max %>%
   group_by(fish, speed) %>%
@@ -48,8 +56,8 @@ pseed.sum.max<- pseed.max %>%
 pseed.sum.max%>%
   ggplot(aes(x=speed,y=amp.sum.mean))+geom_point()+geom_smooth(method="lm")
 
+
 #Create a custom function that computes the standard error of the mean (SE). [see below] and add a column in your summary table pseed.sum.max for SE and call it amp.sum.se.
-#Using ggplot, plot the amp.sum.mean vs. specific swimming speed and add error bars that correspond to the SE of amp.sum. Be sure to color your points and error bars by specimen (fish).
 
 StanErr <- function(x){
   sd(x)/ sqrt(length(x))
@@ -63,10 +71,16 @@ pseed.sum.max <- pseed.sum.max %>%
   left_join(pseed.sum.se, by = c("speed","fish")) %>%
   print()
 
-pseed.sum.max%>%
-  ggplot(aes(x=amp.sum.mean,y=speed))+geom_point()+geom_smooth(method="lm")
+#Using ggplot, plot the amp.sum.mean vs. specific swimming speed and add error bars that correspond to the SE of amp.sum. Be sure to color your points and error bars by specimen (fish).
+
+pseed.sum.max %>%
+  ggplot(aes(x=speed,y=amp.sum.mean,col=fish))+geom_point()+geom_smooth(method="lm")+
+  geom_errorbar(aes(ymin=amp.sum.mean-amp.sum.se, ymax=amp.sum.mean+amp.sum.se), width=0.5, color="skyblue")+theme_linedraw()
 
 
 #Download this file, read it as a tibble and merge it with the your new pseed.sum.max tibble. [see below].
+
+
+
 #Use ggplot to plot the metabolic power output of each fish vs. mean maximum of amp.sum.
 #Include the code for all the tasks listed above in the “groupname_module2.R” script and upload it to this link.
